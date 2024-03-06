19eb457cbf486aca1e3e16ea861cd13803680aae
Axel Girard <axel942@gmail.com>
2024-03-06 20:08:10 +0100

    Feature: Display a list of string with gradualy adding prefixes

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,9 +1,17 @@
-import { displayPrefix, displayFirstPrefix } from "./app";
+import { displayPrefix, displayFirstPrefix, displayAllPrefix } from "./app.ts"
 
 test('display a string of the first prefix', () => {
-    expect(displayFirstPrefix(['c'])).toBe('c');
+  expect(displayFirstPrefix(['c'])).toStrictEqual('c')
 })
 
 test('display a string of the first two prefixes', () => {
-    expect(displayPrefix(['c', 'n'])).toBe('cn');
+  expect(displayPrefix(['c', 'n'])).toStrictEqual('cn')
+})
+
+test('display an array of string for the first two input', () => {
+  expect(displayAllPrefix(['c', 'n'])).toStrictEqual(['c', 'cn'])
+})
+
+test('display an array of string for the first three input', () => {
+  expect(displayAllPrefix(['c', 'n', 's'])).toStrictEqual(['c', 'cn', 'cns'])
 })
