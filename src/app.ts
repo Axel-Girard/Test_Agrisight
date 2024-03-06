a22ac25546b74d7e14cb82b2e31f8dc4f8c9e32a
Axel Girard <axel942@gmail.com>
2024-03-06 19:07:56 +0100

    Feature: TDD display first two letters of the prefixes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-export const displayFirstPrefix = (prefixes: string[]): string => {
-  return prefixes[0];
-};
+function displayPrefix(prefixes: string[]): string {
+  let message: string = ""
+  prefixes.map((value: string) => message += value)
+
+  return message
+}
+
+function displayFirstPrefix(prefixes: string[]): string {
+  return prefixes[0]
+}
+
+displayPrefix(['c', 'n'])
+
+export { displayPrefix, displayFirstPrefix }
