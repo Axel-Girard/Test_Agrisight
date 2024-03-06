425d8a49a7ec069fe34c8c4b1304b6ab05c493c2
Axel Girard <axel942@gmail.com>
2024-03-06 19:07:56 +0100

    Feature: TDD display first two letters of the prefixes

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,9 @@
-import { displayFirstPrefix } from "./app";
+import { displayPrefix, displayFirstPrefix } from "./app";
 
 test('display a string of the first prefix', () => {
     expect(displayFirstPrefix(['c'])).toBe('c');
+})
+
+test('display a string of the first two prefixes', () => {
+    expect(displayPrefix(['c', 'n'])).toBe('cn');
 })
