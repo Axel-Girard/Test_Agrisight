1602087cb5794b6f2644c41894b0e41a34575db3
Axel Girard <axel942@gmail.com>
2024-03-07 10:09:43 +0100

    Feature: TDD: Display the 4th first row

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,17 +1,26 @@
-import { displayPrefix, displayFirstPrefix, displayAllPrefix } from "./app.ts"
+import {
+  displayPrefixAsString,
+  displayFirstPrefix,
+  displayAllPrefixAsString,
+  displayListPrefix,
+} from "./app.ts"
 
 test('display a string of the first prefix', () => {
   expect(displayFirstPrefix(['c'])).toStrictEqual('c')
 })
 
 test('display a string of the first two prefixes', () => {
-  expect(displayPrefix(['c', 'n'])).toStrictEqual('cn')
+  expect(displayPrefixAsString(['c', 'n'])).toStrictEqual('cn')
 })
 
-test('display an array of string for the first two input', () => {
-  expect(displayAllPrefix(['c', 'n'])).toStrictEqual(['c', 'cn'])
+test('display an array of string for the first 2 inputs', () => {
+  expect(displayAllPrefixAsString(['c', 'n'])).toStrictEqual(['c', 'cn'])
 })
 
-test('display an array of string for the first three input', () => {
-  expect(displayAllPrefix(['c', 'n', 's'])).toStrictEqual(['c', 'cn', 'cns'])
+test('display an array of string for the first 3 inputs', () => {
+  expect(displayAllPrefixAsString(['c', 'n', 's'])).toStrictEqual(['c', 'cn', 'cns'])
+})
+
+test('display an array of string for the first 4 inputs', () => {
+  expect(displayListPrefix(['c', 'n', 's'])).toStrictEqual(['c', 'cn', 'cns', 'cs'])
 })
