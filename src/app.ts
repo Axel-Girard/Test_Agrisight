bdc38db957e4b3649bb4db95c4971396432b3e60
Axel Girard <axel942@gmail.com>
2024-03-07 11:36:54 +0100

    Feature: Finish test to display a generated array of string

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,52 @@
 function displayListPrefix(prefixes: string[]): string[] {
   const message: string[] = []
-  let tempPrefixes: string[] = prefixes
+  let tempPrefixes: string[] = []
 
-  message.push(...displayAllPrefixAsString(tempPrefixes))
-  tempPrefixes.splice(tempPrefixes.length - 2, 1)
-  message.push(displayPrefixAsString(tempPrefixes))
+  tempPrefixes.push(...prefixes);
+
+  for (let i = 0; i < prefixes.length; i++) {
+    message.push(...displayUpgradingPrefixAsString(tempPrefixes))
+    message.push(...displayDowngradingPrefixAsString(tempPrefixes[0], tempPrefixes.toSpliced(0, 2)))
+    tempPrefixes.shift()
+  }
 
   return message
 }
 
-function displayAllPrefixAsString(prefixes: string[]): string[] {
+/**
+ * Return an array of string with a prefix and losing the second letter
+ * @param prefixe first letter of each word that never change
+ * @param sufixes word losing it's first letter every loop
+ * @returns 
+ */
+function displayDowngradingPrefixAsString(prefixe: string, sufixes: string[]): string[] {
+  const message: string[] = []
+
+  for (let i = sufixes.length - 1; i >= 0; i--) {
+    message.push(prefixe + sufixes.join(''))
+    sufixes.shift()
+  }
+
+  return message
+}
+
+/**
+ * Return an array of string that get a letter from the prefix on every loop
+ * @param prefixes letter creating a word
+ * @returns 
+ */
+function displayUpgradingPrefixAsString(prefixes: string[]): string[] {
   const message: string[] = []
   let tempPrefixes: string[] = []
 
   prefixes.map((value: string) => {
     tempPrefixes.push(value)
-    message.push(displayPrefixAsString(tempPrefixes))
+    message.push(tempPrefixes.join(''))
   })
 
   return message
 }
 
-function displayPrefixAsString(prefixes: string[]): string {
-  let message: string = ""
-  prefixes.map((value: string) => message += value)
+console.log(displayListPrefix(['c']))
 
-  return message
-}
-
-function displayFirstPrefix(prefixes: string[]): string {
-  return prefixes[0]
-}
-
-export { displayAllPrefixAsString, displayPrefixAsString, displayFirstPrefix, displayListPrefix }
+export { displayUpgradingPrefixAsString, displayListPrefix }
